e282964bda704475de66cfc126b14d6f87e441b2
gabriellaec <49689481+gabriellaec@users.noreply.github.com>
2020-03-31 12:17:56 -0300

    le_scan

--- projeto_ros/exemplos_python/scripts/le_scan.py
+++ projeto_ros/exemplos_python/scripts/le_scan.py
@@ -10,6 +10,11 @@ from sensor_msgs.msg import LaserScan
 
 
 def scaneou(dado):
+	#global dist
+	d = (np.array(dado.ranges)[0]).round(decimals=2)
+	dist.append(d)
+
+
 	print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
 	print("Leituras:")
 	print(np.array(dado.ranges).round(decimals=2))
@@ -17,21 +22,3 @@ def scaneou(dado):
 	#print(np.array(dado.intensities).round(decimals=2))
 
 	
-
-
-if __name__=="__main__":
-
-	rospy.init_node("le_scan")
-
-	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
-	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
-
-
-
-	while not rospy.is_shutdown():
-		print("Oeee")
-		velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 1))
-		velocidade_saida.publish(velocidade)
-		rospy.sleep(2)
-
-
